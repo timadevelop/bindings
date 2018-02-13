8144a15f91a126bce32b67f3935076dd10dde583
vlad <timadevelop@gmail.com>
2018-02-13 21:23:18 +0200

    link readme as help

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -8,7 +8,7 @@ export default class Footer extends React.Component {
     return (
       <footer id="main-footer">
         <a target="_blank" href="https://www.producthunt.com/@teemofeev" className="click btn">product hunt</a>
-        <a target="_blank" href="https://gist.github.com/timadevelop/a908bc3b1ad294bea618e554b59aa074" className="click btn">help</a>
+        <a target="_blank" href="https://github.com/timadevelop/bindings#bindings" className="click btn">help</a>
         <a onClick={() => Popup.queue(contactPopup)} className="click btn">contact me</a>
       </footer>
     )
